130f013c6b4155badaaa985531be3a21613e7758
Surbhi10t <tailorsurbhi10@gmail.com>
2024-08-28 11:30:20 +0530

    js assignment 3

---
+++ A3-Q1-Q5..js
@@ -0,0 +1,31 @@
+// Question 1: 
+
+// let arr=[10,20,30,40];
+// arr.push(100);
+// console.log(arr);
+
+// Question 2:
+
+// let arr2 = [10,20,30,40,50];
+// arr2.pop();
+// console.log(arr2);
+
+// Question 3:
+
+let arr3=[10,20,30,40,50];
+arr3.shift();
+console.log(arr3);
+
+// Question 4:
+
+let arr4 = [10,20,30,40,50];
+arr4.unshift((100));
+console.log(arr4);
+
+// Question 5:
+
+let arr5=[10,20,30,40];
+arr5.splice(1 , 1 , "hello");
+console.log(arr5);
+
+
